c21170861f1d5bd0fa1522eb9e72510c908eca1f
ashish bongu <bonguashish532222@gmail.com>
2026-02-18 13:50:29 +0530

    added js scope

---
+++ JsBasics/1.2JsScope/Sample.js
@@ -0,0 +1,30 @@
+let a=10; // cannot be redeclared in the same scope
+console.log(a);
+
+{
+    let a=40; // this belongs to this block.
+    console.log(a);
+}
+
+console.log(a);//This a doesn't refer to the a that is assigned in the above block, it refers to the global scope.
+
+//This code shows how scope works for let keyword.
+
+add();
+
+function add(){
+    let a=10;
+    let b=20;
+    console.log(a+b);
+}
+
+
+console.log(c)
+var c= 10;
+
+//JS Hoisting: During memory creation phase js moves all variables and function declarations to top of the their scope 
+
+// var: Function Scoped | Hoisted | can be updated / redclared
+// let: Block Scoped    | Hoisted | can be updated / cannot be redeclared in the same scope
+// const: Block Scoped  | Hoisted | cannot be updated / cannot be redeclared 
+
