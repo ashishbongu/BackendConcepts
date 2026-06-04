7c1517b2cfb4ab84d67fc290b5abc13fdf8b80c4
ashish bongu <bonguashish532222@gmail.com>
2026-06-04 23:53:40 +0530

    added new olders in js basics

---
+++ BasicsTest/test1.js
@@ -0,0 +1,26 @@
+let a = 5
+
+function f(){
+    let b = 10
+    console.log("hello")
+}
+
+f();
+
+function place_order(){
+   let boxColor= document.getElementById("color");
+   if(boxColor.style.backgroundColor=="black"){
+    boxColor.style.backgroundColor="green"
+   }else{
+    boxColor.style.backgroundColor="black"
+   }
+}
+
+
+for(var i=1;i<=5;i++){
+    setTimeout(function func(){
+        console.log(i);
+    },i*1000);
+}
+
+
