09b5c4a9d4d3b9b1af0fef31f361fd485a0efd96
ashish bongu <bonguashish532222@gmail.com>
2026-07-03 19:11:38 +0530

    updated with new file

--- BasicsTest/test1.js
+++ BasicsTest/test1.js
@@ -39,6 +39,43 @@ setTimeout(function c(){
     //remember!! time tide and js waits for none :)
 },1000)
 
+//------------------------------------------
+
+let name = "ashish";
+let j=0;
+
+let counter = function(){
+    j++;
+    console.log(j);
+}
+
+counter();
+counter();
+
+
+//------------------------------------------
+
+let add = function(){
+    let x= document.getElementById("num1")
+    let y= document.getElementById("num2")
+    console.log(parseInt(x.value)+parseInt(y.value))
+    document.getElementById("addSol").innerText=parseInt(x.value)+parseInt(y.value)
+}
+
+//-------------------------------------------
+
+function duno(){
+    let p="10";
+    function puno(){
+        console.log("hello world "+p);
+    }
+    return puno
+}
+
+let ret= duno();
+ret()
+
+
 
 
 
